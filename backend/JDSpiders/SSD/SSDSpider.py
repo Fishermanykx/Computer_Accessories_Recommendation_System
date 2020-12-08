0b8bd4d0b34a4a4b2db71efe327b8a0f65c56786
Fisher <fishermanykx.mail@gmail.com>
2020-12-08 08:21:37 +0800

    设计了SSD表的结构

---
+++ backend/JDSpiders/SSD/SSDSpider.py
@@ -0,0 +1,11 @@
+'''
+Description: 
+  interface ：硬盘接口 (M.2/SATA)
+  total_capacity ：总容量 (单位：G)
+  introduction ：商品介绍的 .json
+  Ptable_params ：规格与包装的 .json
+Author: Fishermanykx
+Date: 2020-12-07 13:26:48
+LastEditors: Fishermanykx
+LastEditTime: 2020-12-07 13:29:10
+'''
