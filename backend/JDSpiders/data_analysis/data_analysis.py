be65d32c4fda79e6c7d65582e758c85ac7fffe41
Fisher <fishermanykx.mail@gmail.com>
2020-12-08 11:05:40 +0800

    商品-页面数据分析相关

--- backend/JDSpiders/data_analysis/data_analysis.py
+++ backend/JDSpiders/data_analysis/data_analysis.py
@@ -3,7 +3,7 @@ Description:
 Author: Fishermanykx
 Date: 2020-12-06 18:08:34
 LastEditors: Fishermanykx
-LastEditTime: 2020-12-07 12:26:15
+LastEditTime: 2020-12-08 10:45:49
 '''
 
 import matplotlib.pyplot as plt
@@ -17,8 +17,11 @@ motherboard = [52, 51, 26, 22, 23, 23, 18,
                15, 14, 9, 8, 7, 10, 9, 6, 8, 2, 3, 7, 2]
 video_card = [36, 46, 39, 24, 18, 15, 12,
               14, 3, 4, 3, 4, 1, 8, 3, 7, 2, 0, 6, 1]
+ssd = [50, 52, 47, 50, 36, 36, 31, 26, 18, 22, 15, 16, 20, 9, 17, 13, 7, 4, 10, 6, 7,
+       6, 2, 6, 2, 7, 3, 4, 1, 5, 0, 6, 4, 1, 5, 4, 0, 3, 4, 2, 3, 2, 1, 2, 1, 0, 3, 2, 0, 0]
 y = np.array(motherboard)
 # y = np.array(video_card)
+# y = np.array(ssd)
 x = np.arange(len(y), dtype=int)
 x = x + 1
 y = y / 60
@@ -27,8 +30,9 @@ y = y / 60
 plt.grid(True)
 plt.plot(x, y, '-o')
 # 图像设置
-plt.title("Motherborad")
+plt.title("Motherboard")
 # plt.title("Video Card")
+# plt.title("SSD")
 plt.xlabel("页数")
 plt.ylabel("京东自营商品比例")
 
