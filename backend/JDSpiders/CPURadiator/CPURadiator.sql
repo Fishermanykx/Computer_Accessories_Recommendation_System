5533fe1a44d1756f5460166061475c3065ac5931
Fisher <fishermanykx.mail@gmail.com>
2020-12-08 22:42:37 +0800

    完成CPU散热器爬虫

---
+++ backend/JDSpiders/CPURadiator/CPURadiator.sql
@@ -0,0 +1,16 @@
+use computer_accessories;
+DROP TABLE IF EXISTS `cpu_radiator`;
+CREATE TABLE `cpu_radiator` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `comment_num` varchar(255) DEFAULT NULL,
+  `praise_rate` varchar(255) DEFAULT NULL,
+  `shop_name` varchar(255) DEFAULT NULL,
+  `price` varchar(255) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `introduction` JSON,
+  `Ptable_params` JSON,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4;
+truncate cpu_radiator; 
