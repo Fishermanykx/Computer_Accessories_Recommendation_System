2319b40287264b7bc45101cfeac6b5c41ad59771
Fisher <fishermanykx.mail@gmail.com>
2020-12-08 08:20:59 +0800

    设计了内存表的结构

---
+++ backend/JDSpiders/Memory/MemorySpider.py
@@ -0,0 +1,15 @@
+'''
+Description: 
+  tags ：类别
+  frequency ：内存频率 (单位：MHz)
+  total_capacity ：总容量 (单位：G)
+  memory_num ：内存数量 (单条 or 套条)
+  appearance ：外观特征 (普条/马甲条/RGB灯条)
+  ddr_gen ：DDR代数 (ddr4/ddr3)
+  introduction ：商品介绍的 .json
+  Ptable_params ：规格与包装的 .json
+Author: Fishermanykx
+Date: 2020-12-07 13:12:01
+LastEditors: Fishermanykx
+LastEditTime: 2020-12-07 13:21:14
+'''
