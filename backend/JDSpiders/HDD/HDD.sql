8461ed1187566c54c5c05069bc87d810ddfcde58
Fisher <fishermanykx.mail@gmail.com>
2020-12-08 08:20:18 +0800

    设计了HDD表项的结构

---
+++ backend/JDSpiders/HDD/HDD.sql
@@ -0,0 +1,19 @@
+use computer_accessories;
+DROP TABLE IF EXISTS `hdd`;
+CREATE TABLE `hdd` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `comment_num` varchar(255) DEFAULT NULL,
+  `praise_rate` varchar(255) DEFAULT NULL,
+  `shop_name` varchar(255) DEFAULT NULL,
+  `price` varchar(255) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `tags` varchar(255) DEFAULT NULL,
+  `rotating_speed` varchar(255) DEFAULT NULL,
+  `total_capacity` varchar(255) DEFAULT NULL,
+  `introduction` JSON,
+  `Ptable_params` JSON,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4;
+truncate hdd; 
---
+++ backend/JDSpiders/HDD/HDDSpider.py
@@ -0,0 +1,12 @@
+'''
+Description: 
+  tags ：类别
+  rotating_speed ：硬盘转速 (单位：rpm)
+  total_capacity ：总容量 (单位：G)
+  introduction ：商品介绍的 .json
+  Ptable_params ：规格与包装的 .json
+Author: Fishermanykx
+Date: 2020-12-07 13:24:11
+LastEditors: Fishermanykx
+LastEditTime: 2020-12-07 13:25:57
+'''
